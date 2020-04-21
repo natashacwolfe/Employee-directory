45c7fbbabb478eb25857d7b8ecc603881c768128
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-04-21 13:04:23 -0700

    added employee.js to pages

---
+++ src/components/Wrapper/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Wrapper() {
+  return <p>Hello World!</p>;
+};
+
+export default Wrapper;
---
+++ src/pages/employee.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import API from '../utils/API'
+import Wrapper from '../components/Wrapper';
+import Table from '../components/Table';
+import Row from '../components/Row';
+
+function employee() {
+    const [id, setId] = useState();
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [phone, setPhone] = useState();
+    const [location, setLocation] = useState();
+
+
+}
